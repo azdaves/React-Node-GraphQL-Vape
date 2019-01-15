fe3b72aa1928c7932ece09777a713045519a72fd
Dave Schilling <ds@Daves-MacBook-Pro.local>
2019-01-14 18:24:35 -0700

    final

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schema');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 
@@ -16,6 +16,12 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 
 const PORT = process.env.PORT || 5000;
